90092bca512ee1b48a66cc33df63165bb052cc1d
Chidumam Amadi-Obi <amadiobichidumam@gmail.com>
2026-02-23 08:18:43 +0000

    Delete example_application/config.h

--- example_application/config.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define FILTER_TYPE 0
-
-typedef struct{
-    const float lpf[8] = {0.0310, 0.0810, 0.1480, 0.1880, 0.1880, 0.1480, 0.0810, 0.0310}; // low pass
-    const float hpf[8] = {-0.0220, -0.0570, 0.0730, 0.2570, 0.2570, 0.0730, -0.0570, -0.0220}; // high pass
-    const float dif[8] =  {-0.0250, -0.0500, -0.1000, 0.0000, 0.1000, 0.0500, 0.0250, 0.0000}; // differentiator (detectes rapid changes/ outliers)
-}FilterTypes;
